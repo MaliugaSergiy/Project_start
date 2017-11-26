3748f784f0248030506d5fc2a195e6fe2a30d25d
MaliugaSergii <shtirliy@gmail.com>
2017-11-26 23:54:41 +0200

    product complited, start to fix the first two pagesssd

--- dev/lib/initMap1.js
+++ dev/lib/initMap1.js
@@ -1,6 +1,5 @@
  
 $( document ).ready(function() {
-  console.clear();
 example = document.getElementsByClassName("maps")[0];
 example.addEventListener('onresize', function(){
   example.style.background = 'rgb('+random(0,255)+','+random(0,255)+','+random(0,255)+')';
@@ -16,7 +15,6 @@ var random = function(min, max){
 });
 
 
-
 if (document.getElementById("map1")) {
 	function initMap1() {
 
@@ -27,23 +25,25 @@ if (document.getElementById("map1")) {
 				"elementType": "all",
 				"stylers": [
 					{
-						"hue": "#CC0000"
+						"hue": "#FF0000"
                     }
                 ]
             }
         ];
 
 		mapDraw();
+        
+      
 
 		function mapDraw() {
 			var mainAcadLatLng = {
-				lat: 50.473750,
-				lng: 30.395400
+				lat: 50.451200,
+				lng: 30.470479
 			};
 			//    var mainAcadLatLng = {lat: 50.400322, lng: 30.520159};
 
 			var map = new google.maps.Map(document.getElementById('map1'), {
-				zoom: 17,
+				zoom: 15,
 				disableDefaultUI: true,
 				center: mainAcadLatLng,
 				scrollwheel: false,
@@ -55,7 +55,7 @@ if (document.getElementById("map1")) {
 			});
 
 			var imageMA = {
-				url: 'icons/flag.png',
+				url: 'img/icons/flag1.png',
 				size: new google.maps.Size(74, 103),
 				origin: new google.maps.Point(0, 0),
 				anchor: new google.maps.Point(17, 66),
@@ -72,8 +72,8 @@ if (document.getElementById("map1")) {
 
 
 			var mainAcademyCoordinate = {
-				lat: 50.473750,
-				lng: 30.395400
+				lat: 50.451200,
+				lng: 30.489921
 
 			};
 			var markerMainAcademy = new google.maps.Marker({
@@ -83,11 +83,11 @@ if (document.getElementById("map1")) {
 				draggable: false,
 				//        shape: shape,
 				animation: google.maps.Animation.BOUNCE,
-				title: 'Мебельный салон "Элит"'
+				title: 'Forte Life Insurance'
 			});
 
 			var infowindow = new google.maps.InfoWindow({
-				content: 'Мебельный салон "Элит" '
+				content: 'Forte Life Insurance'
 			});
 
 			markerMainAcademy.addListener('click', function () {
@@ -96,106 +96,10 @@ if (document.getElementById("map1")) {
 			});
 
 			function rerender() {
-				//        if (window.innerWidth < 690) {
-				//            mainAcadLatLng = mainAcademyCoordinate;
-				//            console.log(window.innerWidth);
-				//        }
-
-				var center = map.getCenter();
-				google.maps.event.trigger(map, "resize");
-
-				if (window.innerWidth < 690) {
-					map.setCenter(mainAcademyCoordinate);
-				} else {
-					map.setCenter(mainAcadLatLng);
-				}
-			}
-
-			function rerender2() {
-				var timerId = setInterval(function () {
-					rerender();
-				});
-				setTimeout(function () {
-					clearInterval(timerId);
-				}, 1500);
-			}
-
-			google.maps.event.addDomListener(window, 'resize', rerender);
-			google.maps.event.addDomListener(document.getElementsByClassName('maps')[0], 'mouseover', rerender2);
-			google.maps.event.addDomListener(document.getElementsByClassName('maps')[0], 'mouseout', rerender2);
-
-
-
-		}
-		mapDraw2();
-
-		function mapDraw2() {
-			var mainAcadLatLng = {
-				lat: 47.597175,
-				lng: 37.498171
-
-
-			};
-			//    var mainAcadLatLng = {lat: 50.400322, lng: 30.520159};
-
-			var map = new google.maps.Map(document.getElementById('map2'), {
-				zoom: 17,
-				disableDefaultUI: true,
-				center: mainAcadLatLng,
-				scrollwheel: false,
-				navigationControl: false,
-				mapTypeControl: false,
-				scaleControl: false,
-				//        draggable: false,
-				styles: styleMap
-			});
-
-			var imageMA = {
-				url: 'icons/flag.png',
-				size: new google.maps.Size(74, 103),
-				origin: new google.maps.Point(0, 0),
-				anchor: new google.maps.Point(17, 66),
-				scaledSize: new google.maps.Size(37, 51)
-			};
-			//    var imageMA = {
-			//        path: google.maps.SymbolPath.CIRCLE,
-			//        scale: 1
-			//    };
-			var shape = {
-				coords: [1, 1, 1, 20, 18, 20, 18, 1],
-				type: 'poly'
-			};
-
-
-			var mainAcademyCoordinate = {
-				lat: 47.597175,
-				lng: 37.498171
-
-			};
-			var markerMainAcademy = new google.maps.Marker({
-				position: mainAcademyCoordinate,
-				map: map,
-				icon: imageMA,
-				draggable: false,
-				//        shape: shape,
-				animation: google.maps.Animation.BOUNCE,
-				title: 'Мебельный салон "Элит" '
-			});
-
-			var infowindow = new google.maps.InfoWindow({
-				content: 'Мебельный салон "Элит" '
-			});
-
-			markerMainAcademy.addListener('click', function () {
-				infowindow.open(map, markerMainAcademy);
-
-			});
-
-			function rerender() {
-				//        if (window.innerWidth < 690) {
-				//            mainAcadLatLng = mainAcademyCoordinate;
-				//            console.log(window.innerWidth);
-				//        }
+				        if (window.innerWidth < 690) {
+				            mainAcadLatLng = mainAcademyCoordinate;
+				            console.log(window.innerWidth);
+				        }
 
 				var center = map.getCenter();
 				google.maps.event.trigger(map, "resize");
@@ -221,102 +125,5 @@ if (document.getElementById("map1")) {
 			google.maps.event.addDomListener(document.getElementsByClassName('maps')[0], 'mouseout', rerender2);
 
 		}
-
-		mapDraw3();
-
-		function mapDraw3() {
-			var mainAcadLatLng = {
-				lat: 47.780470,
-				lng: 37.249257
-
-
-			};
-			//    var mainAcadLatLng = {lat: 50.400322, lng: 30.520159};
-
-			var map = new google.maps.Map(document.getElementById('map3'), {
-				zoom: 17,
-				disableDefaultUI: true,
-				center: mainAcadLatLng,
-				scrollwheel: false,
-				navigationControl: false,
-				mapTypeControl: false,
-				scaleControl: false,
-				//        draggable: false,
-				styles: styleMap
-			});
-
-			var imageMA = {
-				url: 'icons/flag.png',
-				size: new google.maps.Size(74, 103),
-				origin: new google.maps.Point(0, 0),
-				anchor: new google.maps.Point(17, 66),
-				scaledSize: new google.maps.Size(37, 51)
-			};
-			//    var imageMA = {
-			//        path: google.maps.SymbolPath.CIRCLE,
-			//        scale: 1
-			//    };
-			var shape = {
-				coords: [1, 1, 1, 20, 18, 20, 18, 1],
-				type: 'poly'
-			};
-
-
-			var mainAcademyCoordinate = {
-				lat: 47.780470,
-				lng: 37.249257
-
-			};
-			var markerMainAcademy = new google.maps.Marker({
-				position: mainAcademyCoordinate,
-				map: map,
-				icon: imageMA,
-				draggable: false,
-				//        shape: shape,
-				animation: google.maps.Animation.BOUNCE,
-				title: 'Мебельный салон "Элит" '
-			});
-
-			var infowindow = new google.maps.InfoWindow({
-				content: 'Мебельный салон "Элит" '
-			});
-
-			markerMainAcademy.addListener('click', function () {
-				infowindow.open(map, markerMainAcademy);
-
-			});
-
-			function rerender() {
-				//        if (window.innerWidth < 690) {
-				//            mainAcadLatLng = mainAcademyCoordinate;
-				//            console.log(window.innerWidth);
-				//        }
-
-				var center = map.getCenter();
-				google.maps.event.trigger(map, "resize");
-
-				if (window.innerWidth < 690) {
-					map.setCenter(mainAcademyCoordinate);
-				} else {
-					map.setCenter(mainAcadLatLng);
-				}
-			}
-
-			function rerender2() {
-				var timerId = setInterval(function () {
-					rerender();
-				});
-				setTimeout(function () {
-					clearInterval(timerId);
-				}, 1500);
-			}
-
-			google.maps.event.addDomListener(window, 'resize', rerender);
-			google.maps.event.addDomListener(document.getElementsByClassName('maps')[0], 'mouseover', rerender2);
-			google.maps.event.addDomListener(document.getElementsByClassName('maps')[0], 'mouseout', rerender2);
-
-		}
-
-
 	}
 }
